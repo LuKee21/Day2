92414f6b12f07f2d3b96ba5efa35d886d2689e81
LuKee21 <gam3rlinfo@gmail.com>
2022-09-21 00:23:58 +0400

    second commit

---
+++ Day2.py
@@ -0,0 +1,9 @@
+#Info about me
+name = "Luke"
+surname = "Dzowenidze"
+age = 13
+height = "168"
+knows_programming = "Little bit"
+
+
+print("Name: " + name + "; Surname: " + surname + "; Age: " + str(age) + "; Height: " + height + "; Knows Programming: " + knows_programming)
